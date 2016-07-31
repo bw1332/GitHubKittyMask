59358b2f317254974e6ffc2c630c8c6153d531ce
Bo Wang <bw1332@nyu.edu>
2016-07-30 22:09:22 -0400

    two cat

--- mask.py
+++ mask.py
@@ -23,26 +23,14 @@ col = len(kitty[0])
 
 dayOfWeek = datetime.now().weekday()
 if dayOfWeek == 5:
-    dayOfWeek = 0
+    dayOfWeek = 7
 elif dayOfWeek == 6:
     dayOfWeek = 1
 else:
-    dayOfWeek = dayOfWeek + 1
-days = dayOfWeek + 7 * 6
+    dayOfWeek = dayOfWeek + 2
+days = dayOfWeek + 7 * 35
 
 if os.path.exists("kitty"):
-#right cat
-    for c in range(col):
-        for r in range(row):
-            times = kitty[row - r - 1][col - c - 1]
-            while times > 0:
-                times -= 1
-                commit(days)
-            days += 1
-
-#left cat
-    days += 7 * 5
-
     for c in range(col):
         for r in range(row):
             times = kitty[row - r - 1][c]
